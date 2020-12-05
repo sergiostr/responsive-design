22e5665dd3ddc902815b0232421b79f5496bf1e0
web-dev11 <chalesquist@gmail.com>
2020-12-05 04:19:09 +0100

    initial commit

---
+++ index.php
@@ -0,0 +1,69 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>proteinfusion.com | Protein Switches Research</title>
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="assets/css/bootstrap.min.css">
+    <link rel="stylesheet" href="assets/css/style.css">
+    <link rel="icon" type="image/x-icon" href="favicon.ico" />    
+</head>
+<body>
+    <header>
+        <nav class="navbar navbar-expand-sm fixed-top">
+            <ul class="navbar-nav">
+                <li class="nav-item">
+                    <a href="#" >
+                        <img class="mini" src="assets/media/mini-logo.png" alt="Logo">
+                    </a>
+                    <a class="qr-img" href="#">
+                        <img class="mini" src="assets/media/QR-icon.png" alt="QR-code">
+                    </a> 
+                </li>
+            </ul>
+        </nav>
+    </header>
+    <div class="container">
+        <div class="row">
+            <div class="content-wrap">
+                <div class="video-wrap">
+                    <video role="presentation" preload="auto" playsinline="" crossorigin="anonymous" loop="" muted="" width="2057" height="1157" autoplay="" src="assets/media/file.mp4"></video>
+                </div>
+                <div class="content text-center">
+                    <img alt="logo.png" src="assets/media/logo.png">
+                </div>
+            </div>
+        </div>
+        <div class="row d-flex justify-content-center media-items">
+            <div class="media-item">
+                <img src="assets/media/icons.png" alt="icons">
+                <div class="stripe d-flex justify-content-center">
+                    <p>Stripe/Text Overlay</p>
+                </div>
+            </div>
+            <div class="media-item">
+                <img src="assets/media/icons.png" alt="icons">
+            </div>
+            <div class="media-item">
+                <img src="assets/media/icons.png" alt="icons">
+            </div>
+            <div class="media-item">
+                <img src="assets/media/icons.png" alt="icons">
+            </div>
+            <div class="media-item">
+                <img src="assets/media/icons.png" alt="icons">
+            </div>
+        </div>
+        <div class="email text-center">
+            <a class="d-inline-block" href="mailto:info@proteinfusion.com">info@proteinfusion.com</a>
+        </div>
+    </div>
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script type="text/javascript" src="assets/js/jquery-3.4.1.min.js"></script>
+    <script type="text/javascript" src="assets/js/bootstrap.min.js"></script>
+    <script type="text/javascript">
+   
+    </script>
+</body></html>
