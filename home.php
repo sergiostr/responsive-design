cff824034e1d7bba7796682a292dab4af8b7fef2
web-dev11 <chalesquist@gmail.com>
2020-12-05 17:20:45 +0100

    complete pdf view page

---
+++ home.php
@@ -0,0 +1,78 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="Protein Switched Research">
+    <meta name="keywords" content="HTML, CSS, PHP, XML, JavaScript">
+    <meta name="author" content="Dejan Pajik">
+    <title>proteinfusion.com | Login</title>
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="assets/css/bootstrap.min.css">
+    <link rel="stylesheet" href="assets/css/main.css">
+    <link rel="stylesheet" href="assets/css/home.css">
+    <link rel="icon" type="image/x-icon" href="favicon.ico">    
+    </head>
+    <!-- Body -->
+    <body>
+        <!-- Header -->
+        <header>
+            <nav class="navbar navbar-expand-sm fixed-top">
+                <ul class="navbar-nav">
+                    <li class="nav-item">
+                        <!-- Mini logo -->
+                        <a href="#" >
+                            <img class="mini" src="assets/media/mini-logo.png" alt="Logo">
+                        </a>
+                        <!-- Logout button -->
+                        <a class="qr-img" href="#">
+                            <img class="mini" src="assets/media/logout-icon.png" alt="logout">
+                        </a> 
+                    </li>
+                </ul>
+            </nav>
+        </header>
+        <!-- End Header -->
+        <!-- Main Body -->
+        <div class="container">
+            <div class="row justify-content-center">
+                <div class="inline panel-view">
+                    <div class="panel-item d-flex justify-content-between">
+                        <p class="text-light">file name</p>
+                        <button class="btn btn-info">view</button>
+                    </div>
+                    <div class="panel-item d-flex justify-content-between">
+                        <p class="text-light">file name</p>
+                        <button class="btn btn-info">view</button>
+                    </div>
+                    <div class="panel-item d-flex justify-content-between">
+                        <p class="text-light">file name</p>
+                        <button class="btn btn-info">view</button>
+                    </div>
+                </div>
+                <div class="panel bg-dark">
+                    <div class="detail">
+                        <h5 class="text-center text-light">Spect 1.pdf</h5>
+                    </div>    
+                </div>
+            </div>
+            
+        </div>
+        <!-- End Main Body -->
+        <!-- Footer -->
+        <footer>
+            <div class="container">
+                <!-- Email address -->
+                <div class="row email justify-content-center">
+                    <a class="d-inline-block" href="mailto:info@proteinfusion.com" style="font-size: 20px">info@proteinfusion.com</a>
+                </div>
+            </div>
+        </footer> 
+        <!-- End Footer -->
+        <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+        <script type="text/javascript" src="assets/js/jquery-3.4.1.min.js"></script>
+        <script type="text/javascript" src="assets/js/bootstrap.min.js"></script>
+    </body>
+    <!-- End Body -->
+</html>
