b2c23857293ce454c94bb2932ba447254cf219d9
web-dev11 <chalesquist@gmail.com>
2020-12-05 22:23:11 +0100

    push now

---
+++ index.php
@@ -0,0 +1,131 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="Protein Switched Research">
+    <meta name="keywords" content="HTML, CSS, PHP, XML, JavaScript">
+    <meta name="author" content="Dejan Pajik">
+    <title>proteinfusion.com | Protein Switches Research</title>
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="assets/css/bootstrap.min.css">
+    <link rel="stylesheet" href="assets/css/main.css">
+    <link rel="icon" type="image/x-icon" href="favicon.ico" />    
+    </head>
+    <!-- Body -->
+    <body>
+        <!-- Header -->
+        <header>
+            <nav class="navbar navbar-expand-sm fixed-top">
+                <ul class="navbar-nav">
+                    <li class="nav-item">
+                        <!-- Mini logo -->
+                        <a href="#" >
+                            <img class="mini" src="assets/media/mini-logo.png" alt="Logo">
+                        </a>
+                        <!-- QR code -->
+                        <a class="qr-img" href="#">
+                            <img class="mini" src="assets/media/QR-icon.png" alt="QR-code">
+                        </a> 
+                    </li>
+                </ul>
+            </nav>
+        </header>
+        <!-- End Header -->
+        <!-- Main Body -->
+        <div class="container">
+            <div class="row">
+                <div class="content-wrap">
+                    <!-- Backgrount Video -->
+                    <div class="video-wrap">
+                        <video role="presentation" preload="auto" playsinline="" crossorigin="anonymous" loop="" muted="" width="2057" height="1157" autoplay="" src="assets/media/file.mp4"></video>
+                    </div>
+                    <!-- Logo -->
+                    <div class="content text-center">
+                        <img alt="logo.png" src="assets/media/logo.png">
+                    </div>
+                </div>
+            </div>
+            <!-- Media Items -->
+            <div class="row d-flex justify-content-center media-items">
+                <!-- Media Item -->
+                <div class="media-item">
+                    <img src="assets/media/camera.png" alt="icons">
+                    <div class="stripe d-flex justify-content-center">
+                        
+                    </div>
+                    <div class="stripe-text">
+                        <p class="text-center">Stripe/Text Overlay</p>
+                    </div>   
+                </div>
+                <!-- End Media Item -->
+                <!-- Media Item -->
+                <div class="media-item">
+                    <img src="assets/media/cal.png" alt="icons">
+                    <div class="stripe d-flex justify-content-center">
+                        
+                    </div>
+                    <div class="stripe-text">
+                        <p class="text-center">Stripe/Text Overlay</p>
+                    </div>
+                </div>
+                <!-- End Media Item -->
+                <!-- Media Item -->
+                <div class="media-item">
+                    <img src="assets/media/cal.png" alt="icons">
+                    <div class="stripe d-flex justify-content-center">
+                        
+                    </div>
+                    <div class="stripe-text">
+                        <p class="text-center">Stripe/Text Overlay</p>
+                    </div>
+                </div>
+                <!-- End Media Item -->
+                <!-- Media Item -->
+                <div class="media-item">
+                    <img src="assets/media/camera.png" alt="icons">
+                    <div class="stripe d-flex justify-content-center">
+                        
+                    </div>
+                    <div class="stripe-text">
+                        <p class="text-center">Stripe/Text Overlay</p>
+                    </div>
+                </div>
+                <!-- End Media Item -->
+                <!-- Media Item -->
+                <div class="media-item">
+                    <img src="assets/media/camera.png" alt="icons">
+                    <div class="stripe d-flex justify-content-center">
+                        
+                    </div>
+                    <div class="stripe-text">
+                        <p class="text-center">Stripe/Text Overlay</p>
+                    </div>
+                </div>
+                <!-- End Media Item -->
+            </div>   
+            <!-- End Media Items -->
+        </div>
+        <!-- End Main Body -->
+        <!-- Footer -->
+        <footer>
+            <div class="container">
+                <!-- Email address -->
+                <div class="email text-center">
+                    <a class="d-inline-block" href="mailto:info@proteinfusion.com">info@proteinfusion.com</a>
+                </div>
+                <!-- Company infor -->
+                <div class="email text-center">
+                    <p><a class="d-inline-block">Company Name</a></p>
+                    <p><a class="d-inline-block">Company Address</a></p>
+                </div>
+            </div>
+        </footer> 
+        <!-- End Footer -->
+        <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+        <script type="text/javascript" src="assets/js/jquery-3.4.1.min.js"></script>
+        <script type="text/javascript" src="assets/js/bootstrap.min.js"></script>
+    </body>
+    <!-- End Body -->
+</html>
---
+++ login.php
@@ -0,0 +1,75 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="Protein Switched Research">
+    <meta name="keywords" content="HTML, CSS, PHP, XML, JavaScript">
+    <meta name="author" content="Dejan Pajik">
+    <title>proteinfusion.com | Login</title>
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="assets/css/bootstrap.min.css">
+    <link rel="stylesheet" href="assets/css/main.css">
+    <link rel="stylesheet" href="assets/css/login.css">
+    <link rel="icon" type="image/x-icon" href="favicon.ico">    
+    </head>
+    <!-- Body -->
+    <body>
+        <!-- Header -->
+        <header>
+            <nav class="navbar navbar-expand-sm fixed-top">
+                <ul class="navbar-nav">
+                    <li class="nav-item">
+                        <!-- Mini logo -->
+                        <a href="#" >
+                            <img class="mini" src="assets/media/mini-logo.png" alt="Logo">
+                        </a>
+                        <!-- QR code -->
+                        <!-- <a class="qr-img" href="#">
+                            <img class="mini" src="assets/media/QR-icon.png" alt="QR-code">
+                        </a>  -->
+                    </li>
+                </ul>
+            </nav>
+        </header>
+        <!-- End Header -->
+        <!-- Main Body -->
+        <div class="container">
+            <div class="row justify-content-center">
+                <div class="panel bg-dark">
+                    <div class="detail">
+                        <h5 class="text-center text-danger">Product ID: ID</h5>
+                        <h5 class="text-center text-danger">Lot ID: ID</h5>
+                    </div>
+                    <br>
+                    <h5 class="text-center text-danger">View Datasheet</h5>
+                    <form action="login.php" type="POST">
+                        <div class="form-group">
+                            <input type="email" class="form-control" id="email" placeholder="Email address">
+                        </div>
+                        <div class="form-group">
+                            <input type="password" class="form-control" id="pwd" placeholder="Password">
+                        </div>
+                        <button type="submit" class="btn btn-primary">Login Button</button>
+                    </form>
+                </div>
+            </div>
+        </div>
+        <!-- End Main Body -->
+        <!-- Footer -->
+        <footer>
+            <div class="container">
+                <!-- Email address -->
+                <div class="row email justify-content-center">
+                    <a class="d-inline-block" href="mailto:info@proteinfusion.com" style="font-size: 20px">info@proteinfusion.com</a>
+                </div>
+            </div>
+        </footer> 
+        <!-- End Footer -->
+        <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+        <script type="text/javascript" src="assets/js/jquery-3.4.1.min.js"></script>
+        <script type="text/javascript" src="assets/js/bootstrap.min.js"></script>
+    </body>
+    <!-- End Body -->
+</html>
