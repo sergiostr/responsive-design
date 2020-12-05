9ad591d0187147dbc6558e3491635312b45deeba
web-dev11 <chalesquist@gmail.com>
2020-12-05 11:06:17 +0100

    complete first page(landing page)

--- index.php
+++ index.php
@@ -9,61 +9,121 @@
     <link rel="stylesheet" href="assets/css/bootstrap.min.css">
     <link rel="stylesheet" href="assets/css/style.css">
     <link rel="icon" type="image/x-icon" href="favicon.ico" />    
-</head>
-<body>
-    <header>
-        <nav class="navbar navbar-expand-sm fixed-top">
-            <ul class="navbar-nav">
-                <li class="nav-item">
-                    <a href="#" >
-                        <img class="mini" src="assets/media/mini-logo.png" alt="Logo">
-                    </a>
-                    <a class="qr-img" href="#">
-                        <img class="mini" src="assets/media/QR-icon.png" alt="QR-code">
-                    </a> 
-                </li>
-            </ul>
-        </nav>
-    </header>
-    <div class="container">
-        <div class="row">
-            <div class="content-wrap">
-                <div class="video-wrap">
-                    <video role="presentation" preload="auto" playsinline="" crossorigin="anonymous" loop="" muted="" width="2057" height="1157" autoplay="" src="assets/media/file.mp4"></video>
+    </head>
+    <body>
+        <!-- Header -->
+        <header>
+            <nav class="navbar navbar-expand-sm fixed-top">
+                <ul class="navbar-nav">
+                    <li class="nav-item">
+                        <!-- Mini logo -->
+                        <a href="#" >
+                            <img class="mini" src="assets/media/mini-logo.png" alt="Logo">
+                        </a>
+                        <!-- QR code -->
+                        <a class="qr-img" href="#">
+                            <img class="mini" src="assets/media/QR-icon.png" alt="QR-code">
+                        </a> 
+                    </li>
+                </ul>
+            </nav>
+        </header>
+        <!-- End Header -->
+        <!-- Main Body -->
+        <div class="container">
+            <div class="row">
+                <div class="content-wrap">
+                    <!-- Backgrount Video -->
+                    <div class="video-wrap">
+                        <video role="presentation" preload="auto" playsinline="" crossorigin="anonymous" loop="" muted="" width="2057" height="1157" autoplay="" src="assets/media/file.mp4"></video>
+                    </div>
+                    <!-- Logo -->
+                    <div class="content text-center">
+                        <img alt="logo.png" src="assets/media/logo.png">
+                    </div>
                 </div>
-                <div class="content text-center">
-                    <img alt="logo.png" src="assets/media/logo.png">
+            </div>
+            <!-- Media Items -->
+            <div class="row d-flex justify-content-center media-items">
+                <!-- Media Item -->
+                <div class="media-item">
+                    <img src="assets/media/camera.png" alt="icons">
+                    <div class="stripe d-flex justify-content-center">
+                        
+                    </div>
+                    <div class="stripe-text">
+                        <p class="text-center">Stripe/Text Overlay</p>
+                    </div>   
+                </div>
+                <!-- End Media Item -->
+                <!-- Media Item -->
+                <div class="media-item">
+                    <img src="assets/media/cal.png" alt="icons">
+                    <div class="stripe d-flex justify-content-center">
+                        
+                    </div>
+                    <div class="stripe-text">
+                        <p class="text-center">Stripe/Text Overlay</p>
+                    </div>
+                </div>
+                <!-- End Media Item -->
+                <!-- Media Item -->
+                <div class="media-item">
+                    <img src="assets/media/cal.png" alt="icons">
+                    <div class="stripe d-flex justify-content-center">
+                        
+                    </div>
+                    <div class="stripe-text">
+                        <p class="text-center">Stripe/Text Overlay</p>
+                    </div>
+                </div>
+                <!-- End Media Item -->
+                <!-- Media Item -->
+                <div class="media-item">
+                    <img src="assets/media/camera.png" alt="icons">
+                    <div class="stripe d-flex justify-content-center">
+                        
+                    </div>
+                    <div class="stripe-text">
+                        <p class="text-center">Stripe/Text Overlay</p>
+                    </div>
+                </div>
+                <!-- End Media Item -->
+                <!-- Media Item -->
+                <div class="media-item">
+                    <img src="assets/media/camera.png" alt="icons">
+                    <div class="stripe d-flex justify-content-center">
+                        
+                    </div>
+                    <div class="stripe-text">
+                        <p class="text-center">Stripe/Text Overlay</p>
+                    </div>
+                </div>
+                <!-- End Media Item -->
+            </div>   
+            <!-- End Media Items -->
+        </div>
+        <!-- End Main Body -->
+        <!-- Footer -->
+        <footer>
+            <div class="container">
+                <!-- Email address -->
+                <div class="email text-center">
+                    <a class="d-inline-block" href="mailto:info@proteinfusion.com">info@proteinfusion.com</a>
+                </div>
+                <!-- Company infor -->
+                <div class="email text-center">
+                    <p><a class="d-inline-block">Company Name</a></p>
+                    <p><a class="d-inline-block">Company Address</a></p>
                 </div>
             </div>
-        </div>
-        <div class="row d-flex justify-content-center media-items">
-            <div class="media-item">
-                <img src="assets/media/icons.png" alt="icons">
-                <div class="stripe d-flex justify-content-center">
-                    <p>Stripe/Text Overlay</p>
-                </div>
-            </div>
-            <div class="media-item">
-                <img src="assets/media/icons.png" alt="icons">
-            </div>
-            <div class="media-item">
-                <img src="assets/media/icons.png" alt="icons">
-            </div>
-            <div class="media-item">
-                <img src="assets/media/icons.png" alt="icons">
-            </div>
-            <div class="media-item">
-                <img src="assets/media/icons.png" alt="icons">
-            </div>
-        </div>
-        <div class="email text-center">
-            <a class="d-inline-block" href="mailto:info@proteinfusion.com">info@proteinfusion.com</a>
-        </div>
-    </div>
-    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
-    <script type="text/javascript" src="assets/js/jquery-3.4.1.min.js"></script>
-    <script type="text/javascript" src="assets/js/bootstrap.min.js"></script>
-    <script type="text/javascript">
-   
-    </script>
-</body></html>
+        </footer> 
+        <!-- End Footer -->
+        <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+        <script type="text/javascript" src="assets/js/jquery-3.4.1.min.js"></script>
+        <script type="text/javascript" src="assets/js/bootstrap.min.js"></script>
+        <script type="text/javascript">
+    
+        </script>
+    </body>
+</html>
